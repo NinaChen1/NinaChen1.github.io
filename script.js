42c1d2ab010c0766848598f820e627131ae5fdfe
NinaChen1 <13850261004@163.com>
2025-12-24 17:12:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ const canvas=document.getElementById('cursorTrail');
 const ctx=canvas.getContext('2d');
 let trail=[]; canvas.width=window.innerWidth; canvas.height=window.innerHeight;
 window.addEventListener('resize',()=>{canvas.width=window.innerWidth; canvas.height=window.innerHeight;});
-window.addEventListener('mousemove',e=>{trail.push({x:e.clientX,y:e.clientY,life:200});});
+window.addEventListener('mousemove',e=>{trail.push({x:e.clientX,y:e.clientY,life:100});});
 function animateTrail(){
   ctx.clearRect(0,0,canvas.width,canvas.height);
   trail.forEach(p=>{ctx.fillStyle=`rgba(255,0,0,${p.life/200})`; ctx.beginPath(); ctx.arc(p.x,p.y,6,0,Math.PI*2); ctx.fill(); p.life--;});
